4b0cab90566e66fa10e2a3fec40b5a9d3ddd82ed
kirakassa <kirakassa21.com>
2025-12-07 16:01:47 +0300

    Add all updates to backend, TypeScript, and configs

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,21 @@
-import Image from "next/image";
-import LandingPage from "./(nonedashboared)/landing/page";
+import Image from "react";
+import HeroSection from "./(nonedashboared)/landing/HeroSection";
+import DiscoverSection from "./(nonedashboared)/landing/DiscoverSection";
+import CallToActionSection from "./(nonedashboared)/landing/CallToActionSection";
+import FooterSection from "./(nonedashboared)/landing/FooterSection";
+import FeaturesSection from "./(nonedashboared)/landing/FeaturesSection";
+import Landing from "./(nonedashboared)/landing/page";
+const landing = ()=>{
+return(
+  <div>
+<HeroSection/>
+<FeaturesSection/>
+<DiscoverSection/>
+<CallToActionSection/>
+<FooterSection/>
 
-export default function Home() {
-  return <div>
-    <LandingPage />
-  </div>;
-}
+  </div>
+);
+
+};
+export default Landing;
---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+/* route import*/
+
+dotenv.config();
+const app=express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+/*route*/
+app.get('/',(req , res) => {
+    res.send("this is home route");
+});
+/*Server*/
+const port= process.env.PORT||3002;
+app.listen(port, () =>{
+    console.log(`server running on port ${port}`);
+})
