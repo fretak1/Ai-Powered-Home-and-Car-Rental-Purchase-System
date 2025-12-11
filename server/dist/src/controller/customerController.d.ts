6e265f9bf660e367db1054bd51e1bef4f1fb38d7
Fretak <frezertakele1@gmail.com>
2025-12-11 13:59:06 +0300

    updated code

--- server/dist/src/controller/customerController.d.ts
+++ server/dist/src/controller/customerController.d.ts
@@ -1,2 +1,4 @@
-export {};
+import type { Response } from "express";
+import type { AuthenticatedRequest } from "../middleware/authMiddleware.js";
+export declare const getCustomer: (req: AuthenticatedRequest, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
 //# sourceMappingURL=customerController.d.ts.map
--- server/dist/src/router/customerRoutes.js
+++ server/dist/src/router/customerRoutes.js
@@ -1,7 +1,20 @@
 import express from "express";
 import { login, register } from "../controller/authController.js";
+import { getCustomer,
+// createCustomer,
+// updateCustomer,
+// getCurrentResidences,
+// addFavoriteProperty,
+// removeFavoriteProperty,
+ } from "../controller/customerController.js";
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
+router.get("/:id", getCustomer);
+// router.put("/:id", updateCustomer);
+// router.post("/", createCustomer);
+// router.get("/:id/current-residences", getCurrentResidences);
+// router.post("/:id/favorites/:propertyId", addFavoriteProperty);
+// router.delete("/:id/favorites/:propertyId", removeFavoriteProperty);
 export default router;
 //# sourceMappingURL=customerRoutes.js.map
