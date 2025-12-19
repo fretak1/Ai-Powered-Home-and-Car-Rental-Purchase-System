870a9a25683e7dc28d67f5f80b9837c1b4dd9806
kirakassa <kirakassa21.com>
2025-12-20 01:00:41 +0300

    Update client and server features

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,49 +1,41 @@
+import express from "express";
 import dotenv from "dotenv";
-import express from "express"
 import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import authRoutes from "./router/authRoutes.js";
-import customerRoutes from "./router/customerRoutes.js";
-import providerRoutes from "./router/providerRoutes.js";
-import { authenticateJwt } from "./middleware/authMiddleware.js";
-import propertyRoutes from "./router/propertyRoutes.js";
+import { authMiddleware } from "./middleware/authMiddleware";
+/* ROUTE IMPORT */
+import tenantRoutes from "./routes/tenantRoutes";
+import managerRoutes from "./routes/managerRoutes";
+import propertyRoutes from "./routes/propertyRoutes";
+import leaseRoutes from "./routes/leaseRoutes";
+import applicationRoutes from "./routes/applicationRoutes";
+
+/* CONFIGURATIONS */
 dotenv.config();
 const app = express();
-
-
-const corsOptions = {
-    origin: [
-        "http://localhost:3000",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-
-/*route*/
-app.get('/', (req, res) => {
-    res.send("this is home route");
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.send("This is home route");
 });
 
-app.use("/proberties",propertyRoutes);
-app.use("/customer", authenticateJwt(['customer']), customerRoutes);
-app.use("/provider", authenticateJwt(['provider']), providerRoutes);
+app.use("/applications", applicationRoutes);
+app.use("/properties", propertyRoutes);
+app.use("/leases", leaseRoutes);
+app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
+app.use("/managers", authMiddleware(["manager"]), managerRoutes);
 
-app.use("/api/auth", authRoutes);
-
-/*Server*/
-const port = process.env.PORT || 3002;
-app.listen(port, () => {
-    console.log(`server running on port ${port}`);
-})
+/* SERVER */
+const port = Number(process.env.PORT) || 3002;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
+});
